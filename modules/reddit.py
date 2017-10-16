e56b6afa7c48a479de2339c1071474520662ac69
Bendik E.Dyrli <skandix@datapor.no>
2017-10-16 23:23:08 +0200

    got something to work atleast .. not i can sleep

--- modules/reddit.py
+++ modules/reddit.py
@@ -7,55 +7,58 @@ import re
 youtube = []
 onlyID = []
 
-def musicUrlFinder(thread):
+def musicUrlFinder(thread, cat):
     r = praw.Reddit(client_id=config.r_cli_id
                     ,client_secret=config.r_cli_secret
                     ,user_agent=config.r_user_agent) 
     sub = r.subreddit(thread)
-    
-    for subz in sub.new():
-        if "youtube" in subz.url or "youtu.be" in subz.url:
-            if subz.url not in youtube:
-               youtube.append(subz.url)
+
+    if cat is "new":
+        for subz in sub.new():
+            if "youtube" in subz.url or "youtu.be" in subz.url:
+                if subz.url not in youtube:
+                   youtube.append(subz.url)
+                else:
+                    pass
+
+    elif cat is "hot":
+        for subz in sub.hot():
+            if "youtube" in subz.url or "youtu.be" in subz.url:
+                if subz.url not in youtube:
+                   youtube.append(subz.url)
+                else:
+                    pass
+
+    elif cat is "top":
+        for subz in sub.top():
+            if "youtube" in subz.url or "youtu.be" in subz.url:
+                if subz.url not in youtube:
+                   youtube.append(subz.url)
+                else:
+                    pass
+
+
+def FindYoutubeID():
+
+    output = []
+
+    for k in youtube:
+        loot = re.search(re.compile(r'(?:\/watch.([v=]+[^+]{11})|youtu.be/([^+]{11}))'), k)
+        if loot:
+            if "/watch?v=" in loot.group():
+                watchId = str(loot.group()).replace('/watch?v=', '')
+                if watchId not in output:
+                    output.append(watchId)
+                else:
+                    pass
+
             else:
-                pass
+                if loot.group() not in output:                
+                    output.append(loot.group())
+                else:
+                    pass
 
-def gatherYTIds():
-    ids = []
+    return output
 
-    youtubeString = ''.join(youtube)
-    loot = re.findall(re.compile(r'(?:\/watch.([v=]+[^+]{11})|youtu.be/([^+]{11}))'), youtubeString)
-
-    for strippedID in loot:
-        Youtube = strippedID[0].replace('v=','').strip()
-        Youtu_be = strippedID[1].strip()
-        ids.append(Youtube)
-        ids.append(Youtu_be)
-
-    encodeList = [x.encode('UTF8') for x in ids]
-    for Id in encodeList:
-        Id = Id.rstrip()
-        if Id is "":
-            pass
-        else:
-            onlyID.append(Id)
-    
-    print onlyID
-
-    jsonDump(onlyID)
-
-def jsonDump(listing):
-    with open('youtube.json', 'w') as fp:
-        json.dump(listing, fp)
-
-def startup():
-    global Loaded
-    with open('youtube.json') as data:
-        Loaded = json.load(data) 
-    return "Found {:} ID's from Previous run\nLooking for new ones".format(len(Loaded))
-
-# print startup()
-# while True:
-#    print "RRRRRRunning"
-#    musicUrlFinder('listentothis')
-#    gatherYTIds()
+#musicUrlFinder('listentothis', 'new')
+#FindYoutubeID(youtube)
--- server.py
+++ server.py
@@ -2,29 +2,28 @@ from flask import Flask, request, render_template
 from modules.getRandItem import *
 from modules.getYTstatus import *
 from modules.getID import *
-from modules.reddit import onlyID
-
-import modules.reddit
-
+from modules.reddit import *
 import logging
 import random
 import os
 
 app = Flask(__name__)
 
-ids = ['_8-RMP3UyT8', 'dh-xVv5vTZc','euEDMWjes0E','TKXFBUn6lQA','_lnAtIS4wVg','fHWHJRx4D44','Iq06K70bneE','K2algR1qzPA', 'Ax5Hqg5FmSs', 'dHl6IK27FpE', 'xzYEccyx4ks', 'VaEbPrZfFqE', 'IuJVoy4Yo0w', 'T6JsFLmAKKQ', 'xn0-RBOSsGQ', 'W_wXNpHIwYs', 'p4PBShvrf4k','-6s_eRHYqVM','BKqZD1usygI','OdixqnfcqeA','V5RVP3u6BaY','fciUDhYrty8','ZALAB5lgG0I', 'cuMJH5pNAME', 'dYPPSU9ULFY', '_VEFR7BXXZw', 'm60FqY6Jags', 'j0xTkipsKM8', 'VSSFG_pNPg0', 'Y3jpQkBb36w', 'DC0LV8Uf_-Y', '3r3dwA0a-lY', 'LElTCvDN7Dg','id7_tSVDEG0','rFjcYwU69r8','XZHlhr_JEv4','bcPNOtK0JpA','R0UaiMHI4M4', 'vzxZzIiO84Yh', 'hQI-hko4eDI', 'Tz_iUQQ2hI4', '1T4OIw8fj7c', 'UmH782k3jKU', 'PPdU7_rqbqA', 'lc7wmkDgbaI', '_hedfvz-Rfw', 'G6k8D1Tzv3U', 'yN8TUgkPnbU','yb8nXwDMLb0','jJN1A_DHtsM','YWnE2eBB2mA','LhiaB9VYOYA','l4XCygw5_1o', 'n_3iZzYrLJA', 'eZaJL0g6R0k', 'dOMH9OHWXHE', 'E5SYKfA8K40', '-fWRJc_mzN8', 'I3ZQHVvxXTs', 'BaGikAngvWY', 'EDggq272KJ0', 'LfDrezpidBo', '4j-dv3Aege0', 'TKVS7vr3SqU', '4o2GGY2ybzU', 'hW5ZVy_pWZU', '4RNJ6hX2lEk','tnw1Kd2vhhw', 'bhVgQW3UhFM', 'e3ZBjOgThts', '81UsVPsAqxw', '8eSQrqHXYYM', 't4HaTd3NUNI', 'uJv06cj8nlg', 'EGJ7z7khKyA', 'drWHAvI9yp4', '1j25A_rN3u0', 'yijXnW9o5HM', 'fnKwkDCNjrU', 'gW-ucfCxM28', 'WLKr4RygOUU', 'tVS7b1bh4ts', 'AS2zBIgwui0', 'MAvH0bKWwew']
+#ids = ['_8-RMP3UyT8', 'dh-xVv5vTZc','euEDMWjes0E','TKXFBUn6lQA','_lnAtIS4wVg','fHWHJRx4D44','Iq06K70bneE','K2algR1qzPA', 'Ax5Hqg5FmSs', 'dHl6IK27FpE', 'xzYEccyx4ks', 'VaEbPrZfFqE', 'IuJVoy4Yo0w', 'T6JsFLmAKKQ', 'xn0-RBOSsGQ', 'W_wXNpHIwYs', 'p4PBShvrf4k','-6s_eRHYqVM','BKqZD1usygI','OdixqnfcqeA','V5RVP3u6BaY','fciUDhYrty8','ZALAB5lgG0I', 'cuMJH5pNAME', 'dYPPSU9ULFY', '_VEFR7BXXZw', 'm60FqY6Jags', 'j0xTkipsKM8', 'VSSFG_pNPg0', 'Y3jpQkBb36w', 'DC0LV8Uf_-Y', '3r3dwA0a-lY', 'LElTCvDN7Dg','id7_tSVDEG0','rFjcYwU69r8','XZHlhr_JEv4','bcPNOtK0JpA','R0UaiMHI4M4', 'vzxZzIiO84Yh', 'hQI-hko4eDI', 'Tz_iUQQ2hI4', '1T4OIw8fj7c', 'UmH782k3jKU', 'PPdU7_rqbqA', 'lc7wmkDgbaI', '_hedfvz-Rfw', 'G6k8D1Tzv3U', 'yN8TUgkPnbU','yb8nXwDMLb0','jJN1A_DHtsM','YWnE2eBB2mA','LhiaB9VYOYA','l4XCygw5_1o', 'n_3iZzYrLJA', 'eZaJL0g6R0k', 'dOMH9OHWXHE', 'E5SYKfA8K40', '-fWRJc_mzN8', 'I3ZQHVvxXTs', 'BaGikAngvWY', 'EDggq272KJ0', 'LfDrezpidBo', '4j-dv3Aege0', 'TKVS7vr3SqU', '4o2GGY2ybzU', 'hW5ZVy_pWZU', '4RNJ6hX2lEk','tnw1Kd2vhhw', 'bhVgQW3UhFM', 'e3ZBjOgThts', '81UsVPsAqxw', '8eSQrqHXYYM', 't4HaTd3NUNI', 'uJv06cj8nlg', 'EGJ7z7khKyA', 'drWHAvI9yp4', '1j25A_rN3u0', 'yijXnW9o5HM', 'fnKwkDCNjrU', 'gW-ucfCxM28', 'WLKr4RygOUU', 'tVS7b1bh4ts', 'AS2zBIgwui0', 'MAvH0bKWwew']
 
-@app.route('/',)
+@app.route('/')
 def homepage():
-    
-    rand = getRandItem(ids)
+ 
+    musicUrlFinder('listentothis', 'new')
+    rand = getRandItem(FindYoutubeID())
     yt_url = 'https://www.youtube.com/watch?v={video_id}'.format(video_id=rand)
     print len(rand)
+    print rand
     if len(rand) == 0: 
         return render_template('VideoNotFound.html'), 404
 
     elif (len(rand) != 0) and (getYTstatus(yt_url)):
-        print "[+] i Found id's {0}".format(len(getID(baseURL)))
+        print "[+] i Found id's {0}".format(len(FindYoutubeID()))
         return render_template('index.html', yt_id=rand, yt_name=getTitle(rand)), 200
 
 @app.errorhandler(404)
@@ -32,6 +31,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
+
     app.run(host="0.0.0.0",port=8069 ,debug=True, threaded=True)
     #reddit.musicUrlFinder('listentothis')
     #reddit.gatherYTIds()
