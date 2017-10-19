88d0363c099531e48fb4a85ea2fbde1180465940
Øystein Andreassen <oysta15@student.uia.no>
2017-10-19 18:59:21 +0200

    New ID parser which stores IDs in a set, implemented save and load for the set without using

--- modules/reddit.py
+++ modules/reddit.py
@@ -1,11 +1,11 @@
 import config
 import praw
-import json
 import re
 
 # lists for collected songs.
 youtube = []
-onlyID = []
+onlyID = set()
+SAVEDLOOT = 'pickles/loot.p'
 
 def musicUrlFinder(thread, cat):
     r = praw.Reddit(client_id=config.r_cli_id
@@ -36,7 +36,43 @@ def musicUrlFinder(thread, cat):
                    youtube.append(subz.url)
                 else:
                     pass
+    parseURLs()
 
+def parseURLs():
+    """
+    Parses all the urls in the youtube list, fetching IDs from "nicely formated" youtube URLs and puts them into a
+    set of IDs for later usage. E.g.
+    https://www.youtube.com/watch?v=spu32SLIdlU
+    youtu.be/spu32SLIdlU
+    """
+    for url in youtube:
+        loot = re.search(r'(v=|\.be\/)([\w\d\-]{11})',url)
+        if loot is not None:
+            onlyID.add(loot.group(2))
+    #saveIDs()
+
+def getRandomID():
+    """
+    :return: A random ID from the set onlyID
+    """
+    import random
+    return random.sample(onlyID,1)[0]
+
+def saveIDs():
+    """
+    Saves the IDs in onlyID to disk
+    """
+    import pickle
+    pickle.dump(onlyID, open(SAVEDLOOT,'wb'))
+
+def loadIds():
+    """
+    Loads the IDs into onlyID from disk if it exists
+    """
+    import pickle, os.path
+    if os.path.isfile(SAVEDLOOT):
+        global onlyID
+        onlyID = pickle.load(open(SAVEDLOOT,'rb'))
 
 def FindYoutubeID():
 
