23ba3a53940524830741ee29df399388da167fa7
Bendik E.Dyrli <skandix@datapor.no>
2017-12-03 11:52:14 +0100

    added logging

--- server.py
+++ server.py
@@ -2,8 +2,9 @@
 from modules.reddit import musicUrlFinder, getRandomID
 from flask import Flask, request, render_template
 from modules.youtube import checkVideoById
-import random
-import os
+import logging
+from logging.handlers import RotatingFileHandler
+import json
 
 app = Flask(__name__)
 
@@ -17,15 +18,32 @@ def homepage(rand = "", title= ""):
         rand = getRandomID()
         title = checkVideoById(rand)
 
-    print "[+] Found ID: {0}".format(rand)
-    print "[+] Title: {0}".format(title.encode('utf-8'))
+    app.logger.info("[+] Found ID: {0}".format(rand))
+    app.logger.info("[+] Title: {0}".format(title.encode('utf-8')))
 
     # passing arguments as a list, then you can recall them by index
     return render_template('index.html', args=[rand, title])
+
+@app.route('/status')
+def status():
+    status={
+        'totalID':len(rand)
+    }
+    return json.dumps()
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
+    formatter = logging.Formatter( "%(asctime)s | %(pathname)s:%(lineno)d | %(funcName)s | %(levelname)s | %(message)s ")
+    handler = RotatingFileHandler('logs/app.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.DEBUG)
+    log.addHandler(handler)
     app.run(host="0.0.0.0",port=4020 ,debug=False, threaded=True)
+
+
